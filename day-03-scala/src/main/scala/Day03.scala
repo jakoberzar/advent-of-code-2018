4f5e2b0eb427222dc3d4e5b1385aa3c86ad6e762
Jakob Erzar <jakob.erzar@gmail.com>
2018-12-03 16:28:28 +0100

    Stylistic changes to solutions for day 3.

--- day-03-scala/src/main/scala/Day03.scala
+++ day-03-scala/src/main/scala/Day03.scala
@@ -3,45 +3,42 @@ import scala.io.Source
 object Day03 {
   def main(args: Array[String]): Unit = {
     val input = Source.fromResource("input.txt").getLines().toList
-    star1(input)
-    star2(input)
-  }
-
-  def star1(input: List[String]): Unit = {
     val claims = parseInput(input)
-    val taken = getFabricWithClaims(claims).map(_.count(_ > 1)).sum
 
-    println(taken)
+    star1(claims)
+    star2(claims)
   }
 
-  def star2(input: List[String]): Unit = {
-    val claims: List[Claim] = parseInput(input)
-    val fabric: Array[Array[Int]] = getFabricWithClaims(claims)
+  def star1(claims: List[Claim]): Unit = {
+    val takenInches = getFabricWithClaims(claims)
+      .flatten
+      .count(_ > 1)
 
-    val completeClaim: Claim = claims.find(
-      _.coordinates.forall { case (x, y) => fabric(y)(x) == 1 }
-    ) match {
-      case Some(claim) => claim
-      case None => throw new Exception("Could not find any such claim!")
-    }
+    println(takenInches)
+  }
+
+  def star2(claims: List[Claim]): Unit = {
+    val fabric = getFabricWithClaims(claims)
+
+    val completeClaim = claims
+      .find(_.coordinates.forall { case (x, y) => fabric(y)(x) == 1 })
+      .get
 
     println(completeClaim.id)
   }
 
   case class Claim(id: Int, left: Int, top: Int, width: Int, height: Int) {
-    val verticalRange: Range = top until top + height
-    val horizontalRange: Range = left until left + width
-    def coordinates: IndexedSeq[(Int, Int)] = for {
-      x <- horizontalRange
-      y <- verticalRange
-    } yield (x ,y)
+    val coordinates: IndexedSeq[(Int, Int)] = for {
+      x <- left until left + width
+      y <- top until top + height
+    } yield (x, y)
   }
 
   def parseInput(input: List[String]): List[Claim] = {
-    input.map((line: String) => {
+    input.map(line => {
       val spaces = line.split(" ")
-      val id = spaces(0).substring(1).toInt;
-      val (left, top) = spaces(2).split(",") match  {
+      val id = spaces(0).drop(1).toInt
+      val (left, top) = spaces(2).split(",") match {
         case Array(x, y) => (x.toInt, y.dropRight(1).toInt)
       }
       val (width, height) = spaces(3).split("x") match {
@@ -57,8 +54,7 @@ object Day03 {
 
     for {
       claim <- claims
-      x <- claim.horizontalRange
-      y <- claim.verticalRange
+      (x, y) <- claim.coordinates
     } fabric(y)(x) += 1
 
     fabric
