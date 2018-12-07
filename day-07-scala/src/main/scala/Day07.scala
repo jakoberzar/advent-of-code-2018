1cdeb2a1d2ac1ba26405e558bc512bebf672d15d
Jakob Erzar <jakob.erzar@gmail.com>
2018-12-07 14:30:08 +0100

    Added solutions for day 7 with mutable structures in Scala.

--- day-07-scala/src/main/scala/Day07.scala
+++ day-07-scala/src/main/scala/Day07.scala
@@ -5,45 +5,77 @@ import scala.collection.mutable.ListBuffer
 // TODO: Remove mutation? :)
 // - Look into getters and setters and things
 object Day07 {
-  var nodeMap: mutable.HashMap[String, Node] = mutable.HashMap[String, Node]()
-
   def main(args: Array[String]): Unit = {
     val input = Source.fromResource("input.txt").getLines().toList
-    parseInput(input)
-    star1()
+
+    star1(parseInput(input))
+    star2(parseInput(input))
   }
 
-  def star1(): Unit = {
-    val queue = mutable.ListBuffer() ++ nodeMap.values.filter(_.require.isEmpty).toList
+  def star1(nodes: Iterable[Node]): Unit = {
+    val queue = ListBuffer() ++ nodes.filter(_.require.isEmpty).toList
     val done: ListBuffer[Node] = mutable.ListBuffer()
     while (queue.nonEmpty) {
       val head = queue.minBy(_.id)
       queue -= head
       head.process()
       queue ++= head.next.filter(_.require.isEmpty)
-      println(head.id)
       done += head
     }
     println(done.map(_.id).mkString(""))
-
   }
 
-  def parseInput(input: List[String]): mutable.HashMap[String, Node] = {
+  def star2(nodes: Iterable[Node]): Unit = {
+    val queue = mutable.ListBuffer() ++ nodes.filter(_.require.isEmpty).toList
+    val done: ListBuffer[Node] = mutable.ListBuffer()
+    var time = 0
+    var workers: List[(Int, Option[Node])] = (1 to 5).toList.map(_ => (0, None))
+    while (queue.nonEmpty || workers.exists { case (t, _) => t > 0 }) {
+      workers = workers.map { case (time, task) =>
+        if (time == 1) {
+          val taskVal = task.get
+          taskVal.process()
+          queue ++= taskVal.next.filter(_.require.isEmpty)
+          done += taskVal
+          (0, None)
+        } else if (time > 1) {
+          (time - 1, task)
+        } else {
+          (time, task)
+        }
+      }.map { case (time, task) =>
+        if (time == 0 && queue.nonEmpty) {
+          val head = queue.minBy(_.id)
+          queue -= head
+          (head.getTime, Some(head))
+        } else {
+          (time, task)
+        }
+      }
+
+      time += 1
+    }
+    println(done.map(_.id).mkString(""))
+    println(time - 1)
+  }
+
+  def parseInput(input: List[String]): Iterable[Node] = {
+    val nodeMap = mutable.HashMap[String, Node]()
+
     input
       .map(parseLine)
-      .flatMap { case (a:String, b: String) => List(a, b) }
+      .flatMap { case (a: String, b: String) => List(a, b) }
       .distinct
       .foreach(x => nodeMap += (x -> Node(x)))
 
     input
       .map(parseLine)
-      .foreach(in => {
-        val (from, to) = in
+      .foreach { case (from, to) =>
         nodeMap(from).addNext(nodeMap(to))
         nodeMap(to).addRequisites(nodeMap(from))
-      })
+      }
 
-    nodeMap
+    nodeMap.values
   }
 
   def parseLine(line: String): (String, String) = (line.charAt(5).toString, line.charAt(36).toString)
@@ -74,6 +106,9 @@ object Day07 {
       done = true
       next.foreach(_.removeRequisites(this))
     }
+
+    def getTime: Int = id.charAt(0).toInt - 'A'.toInt + 1 + 60
+
+    override def toString: String = "Node - " + id
   }
 }
-
